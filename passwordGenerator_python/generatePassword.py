6bc47043ed63f9c91e2ecfa0c840225fc1ef5837
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-05-11 14:46:44 +0500

    Complete password generator (More features can be added later)

--- passwordGenerator_python/generatePassword.py
+++ passwordGenerator_python/generatePassword.py
@@ -3,14 +3,22 @@ import secrets
 
 
 def generate_default_password():
-    print("Default Password")
+    default_length = 12
+    default_yes = 'y'
+
+    print("The default generated password will have a length of 12 and will contain all types of characters and digits because strong password is always a good choice!")
+
+    generated_password = generate_custom_password(
+        default_length, default_yes, default_yes, default_yes, default_yes)
+
+    return generated_password
 
 
 def generate_custom_password(length, lowercase, uppercase, special, digits):
-    lowercase_characters = string.ascii_lowercase
-    uppercase_characters = string.ascii_uppercase
-    special_characters = string.punctuation
-    digit_characters = string.digits
+    lowercase_characters = string.ascii_lowercase if lowercase == 'y' else ""
+    uppercase_characters = string.ascii_uppercase if uppercase == 'y' else ""
+    special_characters = string.punctuation if special == 'y' else ""
+    digit_characters = string.digits if digits == 'y' else ""
 
     all_characters = lowercase_characters + \
         uppercase_characters + special_characters + digit_characters
@@ -40,12 +48,14 @@ def generate_custom_password(length, lowercase, uppercase, special, digits):
         list_of_characters.append(ch)
         remaining_length -= 1
 
-    print(list_of_characters)
+    password = "".join(list_of_characters)
+
+    return password
 
 
 def main():
     user_password_choice = input(
-        "Do you want to create a custom password or a default one? ('C' or 'D'): ").lower()
+        "Do you want to create a custom password or a default one? ('C' or 'D'): ").lower().strip()
 
     if user_password_choice == 'c':
         while True:
@@ -57,19 +67,24 @@ def main():
                 continue
 
             want_lowercase_characters = input(
-                "Do you want lowercase characters in your password? ('Y'/'N'): ").lower()
+                "Do you want lowercase characters in your password? ('Y'/'N'): ").lower().strip()
             want_uppercase_characters = input(
-                "Do you want uppercase characters in your password? ('Y'/'N'): ").lower()
+                "Do you want uppercase characters in your password? ('Y'/'N'): ").lower().strip()
             want_special_characters = input(
-                "Do you want special characters in your password? ('Y'/'N'): ").lower()
+                "Do you want special characters in your password? ('Y'/'N'): ").lower().strip()
             want_digits = input(
-                "Do you want digits in your password? ('Y'/'N'): ").lower()
+                "Do you want digits in your password? ('Y'/'N'): ").lower().strip()
 
-            generate_custom_password(password_length, want_lowercase_characters,
-                                     want_uppercase_characters, want_special_characters, want_digits)
+            generated_password = generate_custom_password(password_length, want_lowercase_characters,
+                                                          want_uppercase_characters, want_special_characters, want_digits)
+
+            print(generated_password)
+
             break
     else:
-        generate_default_password()
+        generated_password = generate_default_password()
+
+        print(generated_password)
 
 
 main()
